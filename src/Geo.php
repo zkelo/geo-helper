12241219bfe53de1e8e71c7684a04774c71f7268
Aleksandr Riabov <zkelo@yandex.ru>
2023-05-04 23:17:18 +0300

    Initial commit

---
+++ src/Geo.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace zkelo\helpers;
+
+/**
+ * Helper class to work with GEO coords
+ *
+ * @version 1.0.0
+ * @see https://stackoverflow.com/questions/10053358/measuring-the-distance-between-two-coordinates-in-php
+ */
+class Geo
+{
+    /**
+     * Middle radius of the Earth _(in meters)_
+     */
+    const EARTH_RADIUS = 6371000;
+
+    /**
+     * Distance calculation formula: Vincenti formula
+     *
+     * @see https://en.wikipedia.org/wiki/Vincenty%27s_formulae
+     */
+    const DISTANCE_FORMULA_VINCENTY = 0;
+
+    /**
+     * Distance calculation formula: Haversine formula
+     *
+     * @see https://en.wikipedia.org/wiki/Haversine_formula
+     */
+    const DISTANCE_FORMULA_HAVERSINE = 1;
+
+    /**
+     * Calculates distance between two points
+     *
+     * @param float $latA First point latitude _(in degrees)_
+     * @param float $lonA First point longitude _(in degrees)_
+     * @param float $latB Second point latitude _(in degrees)_
+     * @param float $lonB Second point longitude _(in degrees)_
+     * @param integer $formula Calculation formula _(one of constants that starts with `DISTANCE_FORMULA`; Vincenti formula by default)_
+     * @return float Distance between two points _(in meters)_
+     */
+    public static function distance(float $latA, float $lonA, float $latB, float $lonB, int $formula = self::DISTANCE_FORMULA_VINCENTY): float
+    {
+        $latA = deg2rad($latA);
+        $lonA = deg2rad($lonA);
+
+        $latB = deg2rad($latB);
+        $lonB = deg2rad($lonB);
+
+        $lonDelta = $lonB - $lonA;
+
+        if ($formula == static::DISTANCE_FORMULA_VINCENTY) {
+            $a = pow(cos($latB) * sin($lonDelta), 2) + pow(cos($latA) * sin($latB) - sin($latA) * cos($latB) * cos($lonDelta), 2);
+            $b = sin($latA) * sin($latB) + cos($latA) * cos($latB) * cos($lonDelta);
+
+            $angle = atan2(sqrt($a), $b);
+        } else {
+            $latDelta = $latB - $latB;
+
+            $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) + cos($latA) * cos($latB) * pow(sin($lonDelta / 2), 2)));
+        }
+
+        return $angle * static::EARTH_RADIUS;
+    }
+}
